24e513a61ec6bcfe5c07185a5365d5b2cef79a28
ADNANLH <adnanharrak5@gmail.com>
2024-10-29 00:11:30 +0100

    crud

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/things');
 
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,13 +11,49 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose.connect('mongodb+srv://admin:admin@mern-db.arn98.mongodb.net/?retryWrites=true&w=majority&appName=MERN-DB',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use(express.json());
 
-app.post('/api/stuff', (req, res, next)=>{
-  console.log(req.body);
-  res.status(201).json({
-    message: 'objet cree !'
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
   });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next)=>{
+  Thing.deleteOne({_id: req.params.id})
+  .then(() => res.status(200).json({message: 'objet supprimé !'}))
+  .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/stuff/:id', (req, res, next)=>{
+  Thing.findOne({_id: req.params.id})
+  .then(thing => res.status(200).json(thing))
+  .catch(error => res.status(404).json({error}));
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
 
 app.get('/api/stuff', (req, res, next) => {
   const stuff = [
@@ -40,4 +76,6 @@ app.get('/api/stuff', (req, res, next) => {
   ];
   res.status(200).json(stuff);
 });
+
+
 module.exports = app;
